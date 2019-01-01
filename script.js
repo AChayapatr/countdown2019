b23cca3ca2dd0e697973a900cdcdd083fd62c936
chayapatr <chayapatr.arc@gmail.com>
2019-01-01 23:06:31 +0700

    fix minute format

--- script.js
+++ script.js
@@ -76,9 +76,9 @@ function timeCheck() {
     let min = today.getMinutes()
     //let hour =  Math.floor((Date.now() % 86400000) / 3600000) + 7
     //let min = Math.floor(((Math.floor((Date.now() % 86400000) / 1000)) % 3600) / 60)
-    //let minFormat = min.toString().padStart(2, "0")
+    let minFormat = min.toString().padStart(2, "0")
     let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
-    DOM.timeInfo.innerHTML = `${hour}:${min}`
+    DOM.timeInfo.innerHTML = `${hour}:${minFormat}`
     DOM.dateInfo.innerHTML = `${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`
     let timeParam = 0
     //morning = 0, day = 1, evening = 2, night = 3
